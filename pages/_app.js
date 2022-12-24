7d9d240c33e7242f62ec28471f19657e703e32a5
Riley Mueller <riley.mue@outlook.com>
2022-12-23 21:54:17 -0600

    Working mvp

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import Dashboard from "./Dashboard";
+import Header from "./Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [theme, setTheme] = useState("light"); //default to light mode
+
+    useEffect(()=>{
+      document.body.className = theme;  
+    }, [theme]);
+
+    function toggleTheme() {
+        if (theme === "light") {
+            setTheme("dark");
+        } else {
+            setTheme("light");
+        }
+    }
+
+    
+
+    return (
+        <div>
+            <Header />
+            <Dashboard theme={theme} toggleTheme={toggleTheme} />
+            <hr className="divider"/>
+            <Component {...pageProps} />
+        </div>
+    );
 }
